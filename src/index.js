7bdf9ed0730697efe4c1b83da2bd46aa2ddcfaad
Sbats <simon.bats12@gmail.com>
2016-07-31 19:40:44 +0200

    Update app to insert results of a call in the database

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 const config = require('./config.json');
 const MongoClient = require('mongodb').MongoClient;
 const MarvelService = require('./marvel.js').MarvelService;
-// const ObjectId = require('mongodb').ObjectId;
+const ObjectId = require('mongodb').ObjectId;
 
 const dbUrl = config.dbUrl;
 let dbInstace = null;
@@ -38,21 +38,54 @@ function getDB() {
   return connectDB(dbUrl);
 }
 
-function crawlSeries() {
+function updateASeries(aSeries) {
   return getDB()
-    .then((db) => {
+    .then(db => {
       const seriesCollection = db.collection('series');
-      return marvel.getSeries({})
-        .then(series => series)
+      seriesCollection.find({ marvelId: aSeries.marvelId }).limit(1)
+        .next()
+        .then(result => {
+          if (!result) {
+            return seriesCollection.insertOne(aSeries)
+              .then(inserted => inserted)
+              .catch(err => err);
+          }
+          const id = new ObjectId(result._id);
+          return seriesCollection.updateOne({ _id: id }, aSeries)
+            .then(updated => updated)
+            .catch(err => err);
+        })
         .catch(err => err);
     })
     .catch(err => err);
 }
 
+function crawlSeries() {
+  return marvel.getSeries()
+    .then(seriesResponse => {
+      const series = seriesResponse.data.results;
+
+      for (let index = 0; index < series.length; index++) {
+        const aSeries = series[index];
+        aSeries.marvelId = aSeries.id;
+        delete aSeries.id;
+        updateASeries(aSeries);
+      }
+
+      return series;
+    })
+    .catch(err => err);
+}
+
 crawlSeries()
-  .then(series => {
-    console.log(series);
+  .then(() => {
     closeDB();
+    console.log('updated successfuly database');
+    process.exit();
   })
-  .catch(err => console.error(err));
+  .catch(err => {
+    console.error(`Error while updating database: ${err}`);
+    closeDB();
+    process.exit();
+  });
 
